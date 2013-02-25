2edc68bd02e34de8989137652a23f4e8b1702194
A.J. Venter <ajventer@gmail.com>
2013-02-25 08:57:21 +0200

    Adding setup.py - in progress

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EZDM',
+      version='1.0',
+      description='Simple tools for helping dungeon masters with ADN&D games',
+      author='A.J. Venter',
+      author_email='ajventer@gmail.com',
+      url='https://github.com/ajventer/ezdm',
+      scripts=['dice_roller','mkcs','xp_tool','quick_combat','ezdm','viewchar'],
+      packages=['ezdm.libs'],
+      package_dir={'ezdm.libs':'ezdm.libs'},
+      package_data={'ezdm.libs':'adnd2e'},
+     )
