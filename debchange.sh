f56556ed426623259ea8b3e715aa3e0c04e06fc0
A.J. Venter <aj.venter@voss-solutions.com>
2013-02-26 11:25:58 +0200

    Debian and RPM package creation support in Makefile

---
+++ debchange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PROJECTDIR=`readlink -f $0`
+PROJECTDIR=`dirname $PROJECTDIR`
+CFGFILE="${PROJECTDIR}/.debchange"
+if ! test -r ${CFGFILE} ; then
+        echo -n  "Your full name ? "
+        read DEBFULLNAME
+        echo -n "Your e-mail ? "
+        read DEBEMAIL
+        echo "DEBFULLNAME=\"$DEBFULLNAME\"" > $CFGFILE
+        echo "DEBEMAIL=\"$DEBEMAIL\"" >> $CFGFILE
+fi
+source $CFGFILE
+export DEBFULLNAME
+export DEBEMAIL
+cd $PROJECTDIR
+if test $# -eq 0; then
+        echo "No options or text given !"
+        echo "I take the same options as debchange"
+        exit 1
+else
+        CHANGEOPTS="$*"
+fi
+debchange $CHANGEOPTS
+
+
+
+
