a377183fc1bcbc5348bdcacea9a83622abf8c528
A.J. Venter <ajventer@gmail.com>
2013-03-02 12:30:05 +0200

    Tag builds by branch

--- debchange.sh
+++ debchange.sh
@@ -16,8 +16,9 @@ export DEBEMAIL
 cd $PROJECTDIR
         #If user did not give options - take text from last commit message
 CHANGEOPTS=`git log | grep -vi "^author:" | grep -vi "^commit"  | grep -vi "^date:" | grep -vi "^$" | head -n 1`
-CHANGEOPTS="$* $CHANGEOPTS "
+CHANGEOPTS="$* $CHANGEOPTS"
 BRANCH=`git branch | fgrep '*' | awk '{print $2}'`
+CHANGEOPTS="$CHANGEOPTS --force-distribution $CHANGEOPTS"
 echo $BRANCH
 debchange $BRANCH - $CHANGEOPTS
 debchange -r
