1a3a7701c6d374b6b8770c6934eb83787ba7a42f
AJ Venter <aj.venter@oracle.com>
2014-07-23 11:29:12 +0200

    Fix keys

--- ezdm_libs/util.py
+++ ezdm_libs/util.py
@@ -108,22 +108,23 @@ def writekey(key, value, json):
 def load_icon(icon='blank.png'):
     syspath = get_sys_data('icons')
     userpath = get_user_data('icons')
-    path=''
-    if os.path.exists(os.path.join(syspath,icon)):
-        path=os.path.join(syspath,icon)
-    if os.path.exists(os.path.join(userpath,icon)):
-        path=os.path.join(userpath,icon) #User data overwrites system data if the same file exists in both
-    if path=='':
-        path=os.path.join(syspath,'blank.png')
+    path = ''
+    if os.path.exists(os.path.join(syspath, icon)):
+        path = os.path.join(syspath, icon)
+    if os.path.exists(os.path.join(userpath, icon)):
+        path = os.path.join(userpath, icon)
+    if path == '':
+        path = os.path.join(syspath, 'blank.png')
     return path
 
+
 def find_files(source, needle=None, basename=False, strip=''):
-    matches =  glob(os.path.join(get_user_data(source), needle))
+    matches = glob(os.path.join(get_user_data(source), needle))
     matches += glob(os.path.join(get_sys_data(source), needle))
     if basename:
         matches = [os.path.basename(match) for match in matches]
     if strip:
-        matches = [match.replace(strip,'') for match in matches]
+        matches = [match.replace(strip, '') for match in matches]
     return list(set(matches))
 
 
@@ -132,30 +133,22 @@ def readfile(source, name, json=False):
     if filenames:
         filename = filenames[0]
     if not json:
-        return open(filename,'r').read()
-    return loads(open(filename,'r').read())
+        return open(filename, 'r').read()
+    return loads(open(filename, 'r').read())
 
 
-def smart_input(*args):
-    print "Smart_input: %s" % (args)
-    pass
-
-def say(*args):
-    print "Say: %s" % (args)
-
-    
 def attack_mods():
-    return loads(open(os.path.join(get_user_data('adnd2e'),"attack_mods.json")).read())
+    return loads(open(os.path.join(get_user_data('adnd2e'), "attack_mods.json")).read())
 
-    
 
-def price_in_copper(gold,silver,copper):
-    s=gold*10 + silver
-    c=s*10+copper
+def price_in_copper(gold, silver, copper):
+    s = gold * 10 + silver
+    c = s * 10 + copper
     return c
 
+
 def convert_money(copper):
-    money={"gold":0,"silver":0,"copper":copper}
+    money = {"gold": 0, "silver": 0, "copper": copper}
     while money['copper'] > 10:
         money['silver'] += 1
         money['copper'] -= 10
@@ -163,47 +156,28 @@ def convert_money(copper):
         money['gold'] += 1
         money['silver'] -= 10
     return money
-        
 
-        
-def heal_dice(auto=True):
-        sides=smart_input('Healing dice sides',validentries=dice_list(),integer=True)
-        num=smart_input('Number of healing dice to roll',integer=True)
-        return rolldice(auto,num,sides)        
 
-  
 def dice_list():
-    return ['4','6','8','10','100','12','20']
+    return ['4', '6', '8', '10', '100', '12', '20']
 
-def inrange(key1,key2):
+
+def inrange(key1, key2):
         if '-' in key2:
-            minimum=int(key2.split('-')[0])
-            maximum=int(key2.split('-')[1])
+            minimum = int(key2.split('-')[0])
+            maximum = int(key2.split('-')[1])
         else:
-            minimum=int(key2)
-            maximum=minimum
+            minimum = int(key2)
+            maximum = minimum
         if int(key1) >= minimum and int(key1) <= maximum:
             return True
         else:
             return False
-            
-def rolldice(auto=True,numdice=1,numsides=20,modifier=0,quiet=False,wsgi=False):
-    output=[]
-    if not auto:
-        return smart_input("Roll a %sd%s dice:" %(numdice,numsides),integer=True)
-    else:
-            total=0
-            for I in range(1,numdice +1):
-                    roll=randrange(1,numsides,1)
-                    output.append("Rolled %s %s-sided dice: %s" %(I,numsides,roll))
-                    total=total+roll
-            
-            total+=modifier       
-            output.append("Total rolled: %s (modifier %s)" % (total,modifier))
-            if not quiet:
-                say(output)
-            if wsgi:
-                return (total,'<br>'.join(output))
-            return total
 
 
+def rolldice(auto=True, numdice=1, numsides=20, modifier=0, quiet=False, wsgi=False):
+    total = 0
+    for I in range(1, numdice + 1):
+            roll = randrange(1, numsides, 1)
+            total = total + roll
+    return total
